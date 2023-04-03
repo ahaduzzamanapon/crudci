9d1d8f7d75084559a610a6f0d332fdd8a29f0a50
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-04-03 15:16:50 +0600

    update

--- app/Controllers/crud.php
+++ app/Controllers/crud.php
@@ -1,65 +1,50 @@
 <?php
-// updated
 namespace App\Controllers;
-use App\Models\crudmodel;
+use App\Models\CrudModel;
 
-class crud extends BaseController
+class Crud extends BaseController
 {
     public function index()
     {
-        $details = new crudmodel();
-        $data['details']=$details->findAll();
-        return view('index',$data);
-        // ->with($data);
+        $model = new CrudModel();
+        $data['details'] = $model->findAll();
+        return view('index', $data);   
     }
+    
     public function add()
     {
-        $details = new crudmodel();
+        $model = new CrudModel();
         $data = [
-
-            'name' =>$this->request->getPost('name'),
-            'roll' =>$this->request->getPost('roll'),
+            'name' => $this->request->getPost('name'),
+            'roll' => $this->request->getPost('roll')
         ];
-        $details->save($data);
-        return redirect('crud');
+        $model->save($data);
+        return redirect()->to('/crud');
     }
+    
     public function edit($id = null)
     {
-
-
-        $details = new crudmodel();
-        $data['details']=$details->find($id);
-        return view('edit',$data);
-      
+        $model = new CrudModel();
+        $data['details'] = $model->find($id);
+        return view('edit', $data);    
     }
+    
     public function remove($id = null)
     {
-
-
-        $details = new crudmodel();
-        $data['details']=$details->where('id',$id)->delete($id);
-       
-        return redirect('crud');
-
-      
+        $model = new CrudModel();
+        $model->delete($id);
+        return redirect()->to('/crud');
     }
+    
     public function update()
     {
-
-
-        $details = new crudmodel();
-        $id=$this->request->getPost('id');
-        $data=[
-            "name"=>$this->request->getPost('name'),
-            "roll"=>$this->request->getPost('roll'),
-
+        $model = new CrudModel();
+        $id = $this->request->getPost('id');
+        $data = [
+            'name' => $this->request->getPost('name'),
+            'roll' => $this->request->getPost('roll')
         ];
-       
-        $details->update($id, $data);
-
-
-
-        return redirect('crud');
-      
+        $model->update($id, $data);
+        return redirect()->to('/crud');
     }
 }
